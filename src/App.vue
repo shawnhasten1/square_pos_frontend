c132d5966c170a002df85927b7706e1d68fe69e6
shawn <shawnhasten@gmail.com>
2022-11-06 22:18:31 -0500

    Added in quantity editing and taxes

--- src/App.vue
+++ src/App.vue
@@ -36,9 +36,9 @@
         <h3>Current Sale</h3>
       </div>
       <ul class="receipt">
-        <li class="line_item" v-for="item in receipt.items" :key="item">
+        <li class="line_item" v-for="item in receipt.items" :key="item" @click="editProduct(item)">
           <span style="display:inline-block">{{item.item_data.name}}</span>
-          <span style="display:inline-block; float: right;">${{formatMoney(item.item_data.variations[0].item_variation_data.price_money.amount/100)}}</span>
+          <span style="display:inline-block; float: right;">{{item.quantity}}x ${{formatMoney(item.item_data.variations[0].item_variation_data.price_money.amount/100)}}</span>
         </li>
       </ul>
       <div class="line_item">
@@ -54,6 +54,30 @@
       </div>
     </div>
   </div>
+  <!-- The Modal -->
+  <div id="product_quantity" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="close">&times;</span>
+        <h2>{{edit_key.item_data.name}}</h2>
+      </div>
+      <div class="modal-body">
+        <div class="quantity_btn minus_btn" @click="edit_key.quantity>1 && edit_key.quantity--">
+          <span style="position: relative; left: 37px; top: -12px; font-size: 80px;">-</span>
+        </div>
+        <div>
+          <h1>{{edit_key.quantity}}</h1>
+        </div>
+        <div class="quantity_btn plus_btn" @click="edit_key.quantity++">
+          <span style="position: relative; left: 28px; top: -7px; font-size: 80px;">+</span>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="#" class="btn btn_primary" @click="updateKey()">Update</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -66,6 +90,14 @@ export default {
     return {
         categories:[],
         keys:[],
+        edit_key:{
+            "id": "",
+            "quantity":0,
+            "item_data": {
+                "category_id": "",
+                "name": ""
+            }
+        },
         receipt:{
           total:0,
           discount:0,
@@ -100,33 +132,60 @@ export default {
       axios.get("http://localhost:5000/v1/items/"+key_id)
       .then(response => {
         console.log(response.data);
-        this.receipt.items.push(response.data);
-        
+        var resp_data = response.data;
+        resp_data['quantity'] = 1;
+        this.receipt.items.push(resp_data);
+        this.getTotals();
+      })
+    },
+    editProduct: function(key){
+      this.edit_key = key;
+      this.modal.style.display = "block";
+    },
+    getTotals: function(){
         this.receipt.total = 0;
         this.receipt.tax = 0;
         for(var i = 0; i<this.receipt.items.length; i++){
-          this.receipt.total+=this.receipt.items[i].item_data.variations[0].item_variation_data.price_money.amount;
+          this.receipt.total+=this.receipt.items[i].item_data.variations[0].item_variation_data.price_money.amount*this.receipt.items[i].quantity;
         }
         this.receipt.tax = this.receipt.total*.065;
         this.receipt.total += this.receipt.tax;
-      })
+    },
+    updateKey: function(){
+      for(var i = 0; i<this.receipt.items.length; i++){
+        if(this.receipt.items[i].id == this.edit_key.id){
+          this.receipt.items[i] = this.edit_key;
+        }
+      }
+      this.modal.style.display = "none";
+      this.getTotals();
     },
     charge: function(){
       var receipt_obj = {
-        "order": {
-          "location_id": "L75ER37CRBXNX",
-          "line_items": []
-        },
-        "idempotency_key": "42ab6d1b-0b08-4f69-b97d-945936185197"
+        "line_items": [],
+        "taxes": []
       }
       for(var i = 0; i<this.receipt.items.length; i++){
-        receipt_obj.order.line_items.push({
-          "quantity": "1",
+        receipt_obj.line_items.push({
+          "quantity": this.receipt.items[i].quantity,
           "catalog_object_id": this.receipt.items[i].item_data.variations[0].id,
           "item_type": "ITEM"
         })
+        try{
+          receipt_obj.taxes.push({
+            "catalog_object_id": this.receipt.items[i].item_data.tax_ids[0]
+          })
+        }catch(error){
+          console.log(error);
+        }
       }
       console.log(receipt_obj)
+      
+      axios.post("http://localhost:5000/v1/orders", receipt_obj)
+      .then(response => {
+        console.log(response.data);
+         this.listings = response.data
+      })
     },
     formatMoney: function(amount, decimalCount = 2, decimal = ".", thousands = ",") {
         try {
@@ -145,10 +204,25 @@ export default {
     }
   },
   mounted() {
-    this.refreshCategories()
+    this.refreshCategories();
+    this.modal = document.getElementById("product_quantity");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      this.modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == this.modal) {
+        this.modal.style.display = "none";
+      }
+    }
   }
 }
-
 </script>
 
 <style>
@@ -281,6 +355,11 @@ export default {
     border-bottom:solid 1px rgb(229 229 229 / 52%);
     min-height: 55px;
   }
+  .receipt > .line_item:hover{
+    cursor: pointer;
+    opacity: .8;
+    transition: .3s;
+  }
   .receipt > .line_item:nth-child(even){
    background: grey;
    color: white;
@@ -301,5 +380,128 @@ export default {
     opacity: .8;
     transition: .3s;
     cursor: pointer;
+  }
+
+  .btn{
+    padding: .5rem;
+    border-radius: 3px;
+    color: white;
+    text-decoration: none;
+  }
+  .btn:hover{
+    cursor: pointer;
+  }
+  .btn_primary{
+    background: rgb(41, 131, 248);
+  }
+  .btn_primary:hover{
+    background: rgb(23, 95, 189);
+  }
+  .btn_primary:active{
+    background: rgb(20, 80, 160);
+  }
+
+  .quantity_btn{
+    width:100px;
+    height:100px;
+    border-radius:50px;
+    border:solid 1px rgba(212,212,212,0.75);
+    position:relative;
+    margin-left: 20px;
+    margin-right: 20px;
+    transition: .3s;
+    -webkit-touch-callout: none; /* iOS Safari */
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+          -ms-user-select: none; /* Internet Explorer/Edge */
+              user-select: none; /* Non-prefixed version, currently
+                                    supported by Chrome, Edge, Opera and Firefox */
+  }
+  .minus_btn:hover{
+    background: red;
+    color: white;
+    cursor: pointer;
+  }
+  .plus_btn:hover{
+    background: green;
+    color: white;
+    cursor: pointer;
+  }
+
+  /*MODALS*/
+  .modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  }
+
+  /* Modal Content */
+  .modal-content {
+    position: relative;
+    background-color: #fefefe;
+    margin: auto;
+    padding: 0;
+    border: 1px solid #888;
+    width: 500px;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+    -webkit-animation-name: animatetop;
+    -webkit-animation-duration: 0.4s;
+    animation-name: animatetop;
+    animation-duration: 0.4s
+  }
+
+  /* Add Animation */
+  @-webkit-keyframes animatetop {
+    from {top:-300px; opacity:0} 
+    to {top:0; opacity:1}
+  }
+
+  @keyframes animatetop {
+    from {top:-300px; opacity:0}
+    to {top:0; opacity:1}
+  }
+
+  /* The Close Button */
+  .close {
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  .close:hover,
+  .close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .modal-header {
+    padding: 2px 16px;
+    border-bottom: 1px solid rgba(212,212,212,0.75);
+  }
+
+  .modal-body {    
+    padding: 2px 16px;
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .modal-footer {
+    border-top: 1px solid rgba(212,212,212,0.75);
+    padding: 16px 16px;
+    display: flex;
+    flex-direction: row-reverse;
   }
 </style>
